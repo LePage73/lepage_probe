e6523383a7265f6abfa6bc72d2154328bafefb65
LePage73 <lepage@simbirsk.ru>
2024-07-24 20:18:15 +0400

    Поправил - нули не попадают

--- lesson12 (2_3).py
+++ lesson12 (2_3).py
@@ -1,19 +1,12 @@
 # Стиль кода часть II. Цикл While
-list_ = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
-# первый вариант без break и continue
-print ('Первый вариант')
-i = 0
-while list_[i] >= 0 and i < len(list_):
-    print(list_[i])
-    i += 1
-
-# второй вариант
-print ('Второй вариант')
-list_ = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+list_ = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 while True :
-    if list_[0] >= 0 :
+    if list_[0] == 0 :
+        list_.pop(0)
+        continue
+    if list_[0] > 0 :
         print(list_.pop(0))
         if len(list_) > 0 : continue
     break
