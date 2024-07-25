ea3e59e824837eb5989f678c7dc8fe34f4553807
LePage73 <lepage@simbirsk.ru>
2024-07-25 09:15:16 +0400

    Сдаю

--- lesson16 (3_1).py
+++ lesson16 (3_1).py
@@ -6,9 +6,11 @@ def count_calls():
     calls += 1
 
 def string_info(in_string):
+    count_calls()
     return tuple([len(in_string),in_string.upper(), in_string.lower()])
 
 def is_contains (in_string, in_list):
+    count_calls()
     while len(in_list) > 0:
         if in_string.lower() == in_list.pop(0).lower(): return True
     return False
