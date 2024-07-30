f3117f8096eed4a94cfe78ed339c74deda0c379c
LePage73 <lepage@simbirsk.ru>
2024-07-30 15:34:09 +0400

    Сдаю

--- lesson25 (5_2).py
+++ lesson25 (5_2).py
@@ -1,10 +1,8 @@
 #Домашняя работа по уроку "Специальные методы классов"
-
 class House:
     def __init__(self,name,number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-
     def go_to(self, new_floor):
         if new_floor <= self.number_of_floors:
             for floor in range(1, new_floor + 1):
