5de96643f22c85dd871d6cbc17864e2027444418
LePage73 <lepage@simbirsk.ru>
2024-07-30 12:16:24 +0400

    Сдаю

---
+++ lesson23 (4_2).py
@@ -0,0 +1,9 @@
+# Домашняя работа по уроку "Пространство имен."
+
+def test_function():
+      def  inner_function():
+            print('Я в области видимости функции test_function')
+      inner_function()
+
+test_function()
+# inner_function()   ошибка  name 'inner_function' is not defined. Did you mean: 'test_function'?
