fcfd80f83791adf809f7a7a7a8c1432795e1838b
LePage73 <lepage@simbirsk.ru>
2024-08-01 12:06:06 +0400

    Сдаю

---
+++ lesson31 (6_3).py
@@ -0,0 +1,44 @@
+#Домашнее задание по теме "Множественное наследование"
+
+class Horse:
+    x_distance = 0
+    sound = 'Frrrrr - потому, что я левее в mro()'
+    def run(self, dx):
+        self.x_distance += dx
+
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat'
+    def fly(self, dy):
+        self.y_distance += dy
+
+class Pegasus(Horse, Eagle):
+    def move(self, dx, dy):
+        super().fly(dy)
+        super().run(dx)
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+    def voice(self):
+        print(self.sound)
+
+
+print(Pegasus.mro())
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+p1.fly(5)
+print(p1.get_pos())
+p1.run(5)
+print(p1.get_pos())
+
+p1.voice()
+
+
+
+
+
+
