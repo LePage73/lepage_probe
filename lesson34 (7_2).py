b808642357de80d7ae6c948028ceaa7652633b39
LePage73 <lepage@simbirsk.ru>
2024-08-04 13:20:33 +0400

    Сдаю/

---
+++ lesson34 (7_2).py
@@ -0,0 +1,21 @@
+def custom_write(file_name,*strings):
+    file = open(file_name,"w",encoding='utf-8')
+    str_num = 0
+    dict_ = {}
+    for str_ in strings[0]:
+        file.write(str(str_ + '\n'))
+        str_num += 1
+        dict_.update({(str_num,file.tell()): str_})
+    file.close()
+    return dict_
+
+info = [
+        'Text for tell.',
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!'
+        ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
