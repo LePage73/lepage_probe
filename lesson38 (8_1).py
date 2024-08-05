b799bfb2510b059781d91e82acf2e7f2c6f355dc
LePage73 <lepage@simbirsk.ru>
2024-08-05 14:49:10 +0400

    Сдаю/

---
+++ lesson38 (8_1).py
@@ -0,0 +1,13 @@
+# Домашнее задание по теме "Try и Except"
+
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except: # одна из переменных не число
+        return str(a) + str(b) # переводим оба аргумента в строки и складываем строки
+
+
+# проверка
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
