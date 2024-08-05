2161368757a08da28eb165ea0e6955f740797d5e
LePage73 <lepage@simbirsk.ru>
2024-08-05 14:50:32 +0400

    Сдаю/

--- lesson38 (8_1).py
+++ lesson38 (8_1).py
@@ -7,7 +7,7 @@ def add_everything_up(a, b):
         return str(a) + str(b) # переводим оба аргумента в строки и складываем строки
 
 
-# проверка
+# проверка по заданию
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
