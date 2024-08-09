28c8f7f923faae94412b8d2aff3108f51f951405
LePage73 <lepage@simbirsk.ru>
2024-08-09 16:15:48 +0400

    Сдаю/

---
+++ secundomer.py
@@ -0,0 +1,16 @@
+#  Секундомер, для далнейшей работы понадобится
+from datetime import datetime
+class Timer():
+    def __init__(self, name):
+        self.name = name
+        self.start_time = 0
+        self.stop_time = 0
+
+    def start(self):
+        self.start_time = datetime.now()
+    def stop(self):
+        self.stop_time = datetime.now()
+    def __str__(self):
+        return str(self.stop_time - self.start_time)
+    def current_time(self):
+        return str(datetime.now() - self.start_time)
