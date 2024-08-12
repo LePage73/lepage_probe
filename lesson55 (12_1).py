bde64df26ce04f160ab07a01db79bf4af7ec6956
LePage73 <lepage@simbirsk.ru>
2024-08-12 09:32:31 +0400

    Сдаю /

---
+++ lesson55 (12_1).py
@@ -0,0 +1,43 @@
+# Домашнее задание по теме "Простые Юнит-Тесты"
+import unittest
+
+# Проверяемый класс Runner  ######################################
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+# Тест класса Runner ###########################################
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner_ = Runner('Test1')
+        for _ in range(10): runner_.walk()
+        self.assertEqual(runner_.distance, 50)
+
+
+    def test_run(self):
+        runner_ = Runner('Test2')
+        for _ in range(10): runner_.run()
+        self.assertEqual(runner_.distance, 100)
+
+    def test_challenge(self):
+        runner_1 = Runner('1')
+        runner_2 = Runner('2')
+        for _ in range(10):
+            runner_1.run()
+            runner_2.walk()
+        self.assertNotEqual(runner_1.distance, runner_2.distance)
+
+# Запуск проверки ###############################################
+
+if __name__ == '__main__':
+    unittest.main()
