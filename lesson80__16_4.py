01198191cb82e34f0b8d6113f6a80445f42e3a5a
LePage73 <lepage@simbirsk.ru>
2024-09-04 09:18:43 +0400

    Сдаю/

--- lesson80__16_4.py
+++ lesson80__16_4.py
@@ -22,28 +22,38 @@ class User(BaseModel):
 @app.get('/users') # показывает всю БД пользователей
 async def get_all_users() -> list[User]:
     return db_users
+
 @app.post('/user/{username}/{age}') # добавляет запись в БД пользователей
 async def add_user(username: Annotated[str, Path(min_length=2,max_length=20, description='Введите имя пользователя',
                                                  example='Ян или Example_2: LePage73')],
                    age: Annotated[int, Path(gt=12 , lt= 130, description='Введите возраст', example=55)])-> User:
-    db_users.append(User(id = len(db_users), username = username, age = age)) # добавляем запись с новым индексом
-    return User(id = len(db_users), username = username, age = age)
+    if len(db_users) == 0:
+        user_id = 1
+    else:
+        user_id = db_users[-1].id + 1  # по заданию "id этого объекта будет на 1 больше, чем у последнего в списке users"
+    db_users.append(User(id = user_id, username = username, age = age)) # добавляем новую запись с новым индексом
+    return User(id = user_id, username = username, age = age)
+
 @app.put('/user/{user_id}/{username}/{age}') # обновляет по ID запись пользователя в БД
 async def update_user(user_id: Annotated[int, Path(gt=0, lt=1000, description='Введите ID пользователя', example=123)],
                       username: Annotated[str, Path(min_length=2,max_length=20, description='Введите имя пользователя',
                                                  example='Ян или Example_2: LePage73')],
                       age: Annotated[int, Path(gt=12 , lt= 130, description='Введите возраст', example=55)]) -> User:
+
+    user_idx = [i for i,x in enumerate(db_users) if user_id == x.id] # находми индекс запись с нужным ID
     try:
-        edit_user = db_users[user_id] # ищем пользователя с таким ID
+        edit_user = db_users[user_idx[0]] # выбираем нужного пользователя
         edit_user.username = username # меняем имя
         edit_user.age = age # меняем возраст
     except IndexError:
         raise HTTPException(status_code=404, detail=f'User ID {user_id} was not found')
     return edit_user
+
 @app.delete('/user/{user_id}') # удаляет запись с ключом ID
 async def delete_user(user_id: Annotated[int, Path(gt=0, lt=1000, description='Введите ID пользователя', example=123)]) -> User:
+    user_idx = [i for i, x in enumerate(db_users) if user_id == x.id]  # находми индекс запись с нужным ID
     try:
-        return db_users.pop(user_id)
+        return db_users.pop(user_idx[0])
     except IndexError:
         raise HTTPException(status_code=404, detail=f'User ID {user_id} was not found')
 
