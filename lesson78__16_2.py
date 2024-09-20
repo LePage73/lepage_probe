e537187e194849cb830a4f47f29b4626708511c8
LePage73 <lepage@simbirsk.ru>
2024-09-20 10:34:33 +0400

    Сдаю/

--- lesson78__16_2.py
+++ lesson78__16_2.py
@@ -13,11 +13,15 @@ async def welcome():
 @app.get('/user/admin')
 async def admin_page():
     return 'Вы вошли как администратор'
-@app.get("/user/{username}/{age}")
-async def user_name_age(username: Annotated[str, Path(min_length=3, max_length=20, description='Имя пользователя', example='Оля')],
-                        age: Annotated[int, Path(ge=12,le=120,description='Возраст',example=55)]):
-    return f'Информация о пользователе. Имя: {username}, возраст: {age}'
-@app.get("/user/{user_id}")
-async def user_id(user_id: Annotated[int, Path(ge = 1, le = 1000, description='Enter User ID', example='55')]):
-    return f'Вы вошли как пользователь № {user_id}'
 
+
+@app.get("/user/{username}/{age}")
+async def user_name_age(
+        username: Annotated[str, Path(min_length=3, max_length=20, description='Имя пользователя', example='Оля')],
+        age: Annotated[int, Path(ge=12, le=120, description='Возраст', example=55)]):
+    return f'Информация о пользователе. Имя: {username}, возраст: {age}'
+
+
+@app.get("/user/{user_id}")
+async def user_id(user_id: Annotated[int, Path(ge=1, le=1000, description='Enter User ID', example='55')]):
+    return f'Вы вошли как пользователь № {user_id}'
